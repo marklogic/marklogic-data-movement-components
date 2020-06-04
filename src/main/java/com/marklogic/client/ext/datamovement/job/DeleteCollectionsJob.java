f4d455cf225ca30f8e12a93d49f6595dfc9579a6
Rob Rudin <rob.rudin@marklogic.com>
2020-06-04 10:50:23 -0400

    #20 Fixing constructor usage in DeleteCollectionsJob

--- src/main/java/com/marklogic/client/ext/datamovement/job/DeleteCollectionsJob.java
+++ src/main/java/com/marklogic/client/ext/datamovement/job/DeleteCollectionsJob.java
@@ -8,37 +8,57 @@ import java.util.Arrays;
 
 public class DeleteCollectionsJob extends AbstractQueryBatcherJob {
 
-	private String[] collections;
+    private String[] collections;
 
-	public DeleteCollectionsJob() {
-		super();
-		setRequireWhereProperty(false);
-		addRequiredJobProperty("collections", "Comma-delimited list of collections to delete",
-			value -> setCollections(value.split(",")));
-	}
+    public DeleteCollectionsJob() {
+        super();
+        setRequireWhereProperty(false);
+        addCollectionsProperty(true);
+    }
 
-	public DeleteCollectionsJob(String... collections) {
-		this();
-		setCollections(collections);
-	}
+    /**
+     * When this constructor is used - i.e. the collections are known at the time the job is constructed - then
+     * "collections" does not become a required property, as it's already been set.
+     *
+     * @param collections
+     */
+    public DeleteCollectionsJob(String... collections) {
+        super();
+        setRequireWhereProperty(false);
+        setCollections(collections);
+        if (collections != null && collections.length > 0) {
+            addCollectionsProperty(false);
+        } else {
+            addCollectionsProperty(true);
+        }
+    }
 
-	@Override
-	protected QueryBatcherBuilder newQueryBatcherBuilder() {
-		return new CollectionsQueryBatcherBuilder(collections);
-	}
+    private void addCollectionsProperty(boolean required) {
+        final String message = "Comma-delimited list of collections to delete";
+        if (required) {
+            addRequiredJobProperty("collections", message, value -> setCollections(value.split(",")));
+        } else {
+            addJobProperty("collections", message, value -> setCollections(value.split(",")));
+        }
+    }
 
-	@Override
-	protected String getJobDescription() {
-		return "Deleting collections: " + Arrays.asList(collections);
-	}
+    @Override
+    protected QueryBatcherBuilder newQueryBatcherBuilder() {
+        return new CollectionsQueryBatcherBuilder(collections);
+    }
 
-	public void setCollections(String... collections) {
-		this.collections = collections;
-		this.addUrisReadyListener(new DeleteListener());
-	}
+    @Override
+    protected String getJobDescription() {
+        return "Deleting collections: " + Arrays.asList(collections);
+    }
 
-	@Override
-	protected void addWhereJobProperties() {
-		// These don't apply to this job
-	}
+    public void setCollections(String... collections) {
+        this.collections = collections;
+        this.addUrisReadyListener(new DeleteListener());
+    }
+
+    @Override
+    protected void addWhereJobProperties() {
+        // These don't apply to this job
+    }
 }
--- src/test/java/com/marklogic/client/ext/datamovement/job/DeleteCollectionsTest.java
+++ src/test/java/com/marklogic/client/ext/datamovement/job/DeleteCollectionsTest.java
@@ -9,51 +9,68 @@ import java.util.Properties;
 
 public class DeleteCollectionsTest extends AbstractDataMovementTest {
 
-	@Test
-	public void test() {
-		Properties props = new Properties();
-		props.setProperty("collections", COLLECTION);
+    @Test
+    public void test() {
+        Properties props = new Properties();
+        props.setProperty("collections", COLLECTION);
 
-		DeleteCollectionsJob job = new DeleteCollectionsJob();
-		List<String> messages = job.configureJob(props);
-		assertTrue("This job doesn't require where* properties to be set", messages.isEmpty());
+        DeleteCollectionsJob job = new DeleteCollectionsJob();
+        List<String> messages = job.configureJob(props);
+        assertTrue("This job doesn't require where* properties to be set", messages.isEmpty());
 
-		assertNotNull(client.newDocumentManager().exists(FIRST_URI));
-		assertNotNull(client.newDocumentManager().exists(SECOND_URI));
+        assertNotNull(client.newDocumentManager().exists(FIRST_URI));
+        assertNotNull(client.newDocumentManager().exists(SECOND_URI));
 
-		job.run(client);
+        job.run(client);
 
-		assertNull(client.newDocumentManager().exists(FIRST_URI));
-		assertNull(client.newDocumentManager().exists(SECOND_URI));
-	}
+        assertNull(client.newDocumentManager().exists(FIRST_URI));
+        assertNull(client.newDocumentManager().exists(SECOND_URI));
+    }
 
-	@Test
-	public void usingSimpleQueryBatcherJob() {
-		SimpleQueryBatcherJob job = new SimpleQueryBatcherJob(new DeleteListener());
-		job.setWhereCollections(COLLECTION);
+    @Test
+    public void collectionsSetViaConstructor() {
+        DeleteCollectionsJob job = new DeleteCollectionsJob(COLLECTION);
 
-		assertNotNull(client.newDocumentManager().exists(FIRST_URI));
-		assertNotNull(client.newDocumentManager().exists(SECOND_URI));
+        List<String> messages = job.configureJob(new Properties());
+        assertTrue("This job doesn't require where* properties to be set, and 'collections' shouldn't be required " +
+                "since the collections were specified via the constructor", messages.isEmpty());
 
-		job.run(client);
+        assertNotNull(client.newDocumentManager().exists(FIRST_URI));
+        assertNotNull(client.newDocumentManager().exists(SECOND_URI));
 
-		assertNull(client.newDocumentManager().exists(FIRST_URI));
-		assertNull(client.newDocumentManager().exists(SECOND_URI));
-	}
+        job.run(client);
 
-	/**
-	 * Any job that extends BatcherConfig can be used here for this test, which is just to verify that the jobId and
-	 * jobName properties are applied correctly.
-	 */
-	@Test
-	public void configureJobIdAndName() {
-		Properties props = new Properties();
-		props.setProperty("jobId", "my-job-id");
-		props.setProperty("jobName", "My Job");
+        assertNull(client.newDocumentManager().exists(FIRST_URI));
+        assertNull(client.newDocumentManager().exists(SECOND_URI));
+    }
 
-		DeleteCollectionsJob job = new DeleteCollectionsJob();
-		job.configureJob(props);
-		assertEquals("my-job-id", job.getJobId());
-		assertEquals("My Job", job.getJobName());
-	}
+    @Test
+    public void usingSimpleQueryBatcherJob() {
+        SimpleQueryBatcherJob job = new SimpleQueryBatcherJob(new DeleteListener());
+        job.setWhereCollections(COLLECTION);
+
+        assertNotNull(client.newDocumentManager().exists(FIRST_URI));
+        assertNotNull(client.newDocumentManager().exists(SECOND_URI));
+
+        job.run(client);
+
+        assertNull(client.newDocumentManager().exists(FIRST_URI));
+        assertNull(client.newDocumentManager().exists(SECOND_URI));
+    }
+
+    /**
+     * Any job that extends BatcherConfig can be used here for this test, which is just to verify that the jobId and
+     * jobName properties are applied correctly.
+     */
+    @Test
+    public void configureJobIdAndName() {
+        Properties props = new Properties();
+        props.setProperty("jobId", "my-job-id");
+        props.setProperty("jobName", "My Job");
+
+        DeleteCollectionsJob job = new DeleteCollectionsJob();
+        job.configureJob(props);
+        assertEquals("my-job-id", job.getJobId());
+        assertEquals("My Job", job.getJobName());
+    }
 }
